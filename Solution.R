17221f3f4130b362b491588d3324ac398c87ef6d
Anupam Majhi <majhi.anupam@gmail.com>
2017-09-29 09:10:44 +0530

    Fix line

--- Solution.R
+++ Solution.R
@@ -93,3 +93,12 @@ loanData <- mutate(loanData,revol_util = as.double(strsplit(revol_util,"%")))
 # Correct different representations of NA
 loanData[which(loanData$emp_length == "n/a"),"emp_length"] <- 0
 # Convert to number
+loanData <- mutate(loanData,emp_length = as.numeric(str_replace_all(emp_length,"[a-zA-Z\\+\\s</]","")))
+
+# format date
+
+CorrectDate <- function(x){
+  x <- paste("01-",x,sep = '')
+  yy <- formatC(as.numeric(substr(x,8,9)),width=2,flag=0)
+  if(yy<=17){
+    x <- paste(substr(x,1,7),"20",as.character(yy),sep = "")
