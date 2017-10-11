17f65d059da49179e5de701d57df42816320903d
Anupam Majhi <majhi.anupam@gmail.com>
2017-10-11 09:05:10 +0530

    Update and commit

--- Solution.R
+++ Solution.R
@@ -450,3 +450,12 @@ loanData %>%
 Loan_Status_Summary("revol_util")
 
 loanData %>%
+  ggplot(aes(y=revol_util)) +
+  geom_boxplot(aes(x=loan_status),width=0.6)+
+  stat_summary(geom="text", fun.y=quantile,aes(x=loan_status,label=sprintf("%1.1f", ..y..)),position=position_nudge(x=0.2), size=3.5,vjust = -0.5)
+#higher the utilization of revolving balance, the borrower tends to default much more
+
+
+Loan_Status_Summary("total_acc")
+
+loanData %>%
